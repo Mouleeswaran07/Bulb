331775843cc6b648678f71651694f49d1f5712ca
Mouleeswaran07 <139098916+Mouleeswaran07@users.noreply.github.com>
2023-07-12 10:53:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="./bulb on-off/bulb on.png";
+
+}
+
+function bulb_off(){
+    img.src="./bulb on-off/bulb off.png";
+}
